8b2026865075eef65318365f92e9504e5e3cb631
Joe Diaz <jdiaz128@terpmail.umd.edu>
2023-05-09 05:26:38 -0400

    Reorganized ETL, introduced CPI adjustment
    Moved source datasets into a single folder for organization
    purposes, remove clutter. Also added CPI for usage in normalizing
    for inflation, using CPI-U all-items and averaging annually.
    Plotted tuition by ownership according to normalized cost.

--- ETL/main.py
+++ ETL/main.py
@@ -5,8 +5,8 @@ from plotly.subplots import make_subplots
 
 CollegeScorecard_df_raw = pd.read_csv("YourCollegeData.csv")
 scard_df = CollegeScorecard_df_raw.copy()
-scard_df.rename(
-    mapper={
+scard_df = scard_df.rename(
+    columns={
         "student.size": "size",
         "cost.tuition.in_state": "in_state_tuition",
         "cost.tuition.out_of_state": "out_state_tuition",
@@ -19,26 +19,24 @@ scard_df.rename(
         "school.region_id": "region_id",
         "school.locale": "locale",
         "school.ownership": "ownership"
-    }, inplace=True
+    }
 )
 
-# scard_df["net_cost"] = scard_df.apply(lambda row: 
-#             row["public_net_price"] if (row["ownership"] == 1) else row["private_net_price"],
-#         axis=1
-# )
+
+scard_df["net_cost"] = scard_df.apply(lambda row: 
+            row["public_net_price"] if (row["ownership"] == 1) else row["private_net_price"],
+        axis=1
+)
 
 
-# usn_college_data = pd.read_csv("../usnews_stripped.csv")
-# usn_top_100 = (usn_college_data[usn_college_data["2023"] < 100])[["University Name","IPEDS ID", "2023"]]
+usn_college_data = pd.read_csv("source_data/usnews_stripped.csv")
+usn_top_100 = (usn_college_data[usn_college_data["2023"] < 100])[["University Name","IPEDS ID", "2023"]]
 
-print(scard_df.to_string())
-# fig = go.Figure()
-# fig.add_trace(
-#     go.Violin(
-#         x=scard_df['year'], 
-#         y=scard_df['net_cost'],
-#         box_visible=True,
-#         meanline_visible=True
-#     )
-# )
-# fig.show()
+cpi_df = pd.read_csv("source_data/cpi_all.csv").groupby("Year")["Value"].mean()
+
+print(cpi_df)
+scard_df["net_cost_adjusted"] = scard_df.apply(lambda row: 
+            (row["net_cost"]/cpi_df.at[row["year"]]) * 100,
+        axis=1
+)
+print(scard_df)
