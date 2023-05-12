858d1f4de0da2972530841befcc0d93671a4411b
Joe Diaz <jdiaz128@terpmail.umd.edu>
2023-05-12 09:24:35 -0400

    Data Cleaning but very good

--- ETL/uni_geo_cross.py
+++ ETL/uni_geo_cross.py
@@ -6,6 +6,7 @@ zip_cbsa_tract_df = pd.read_csv('generated_data/ZipToCBSATract.csv')
 
 tract_puma_df = pd.read_table("source_data/2020_Census_Tract_to_2020_PUMA.txt", sep=",").astype('string')
 tract_puma_df.sort_values('TRACTCE')
+tract_puma_df.columns = ['state', 'county', 'tract', 'puma']
 
 school_df['school.zip'] = school_df['school.zip'].str.slice(0,5).astype('int64')
 school_df.sort_values('school.zip')
@@ -35,25 +36,31 @@ school_df = recent_sample
 
 matching_zips_cbsas_df = school_df.merge(zip_cbsa_tract_df[['zip', 'tract', 'cbsa']], left_on='school.zip', right_on='zip')[['school.name', 'zip', 'tract', 'cbsa', 'year', 'school.state_fips', 'county_fips']]
 matching_zips_cbsas_df.columns = ['name', 'zip', 'tract', 'cbsa', 'year', 'desired_state', 'desired_county']
-matching_zips_cbsas_df['tract'] = matching_zips_cbsas_df['tract'].astype(str).str.zfill(11)
-matching_zips_cbsas_df['state'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(0,2)
-matching_zips_cbsas_df['county'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(2,5)
-matching_zips_cbsas_df['tract'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(5,11)
-matching_zips_cbsas_df.sort_values('tract')
+matching_zips_cbsas_df["desired_state"] = matching_zips_cbsas_df['desired_state'].astype(str).str.zfill(2)
+matching_zips_cbsas_df["desired_county"] = matching_zips_cbsas_df['desired_county'].astype(str).str.zfill(3)
 
-final_df = matching_zips_cbsas_df.merge(tract_puma_df, left_on='tract', right_on='TRACTCE')
-final_df['state'] = final_df['STATEFP'].str.zfill(2)
-final_df['county'] = final_df['COUNTYFP'].str.zfill(3)
-final_df['puma'] = final_df['PUMA5CE']
-final_df = final_df.drop(columns=['STATEFP', 'COUNTYFP', 'TRACTCE', 'PUMA5CE'])
-print(final_df.drop_duplicates().to_string())
-final_df = final_df[(final_df['state'].astype('int64') == final_df['desired_state'].astype('int64')) & (final_df['county'].astype('int64') == final_df['desired_county'].astype('int64'))].reset_index(drop=True)
+
+matching_zips_cbsas_df['tract'] = matching_zips_cbsas_df['tract'].astype(str).str.zfill(11)
+matching_zips_cbsas_df['state'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(0,2).str.zfill(2)
+matching_zips_cbsas_df['county'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(2,5).str.zfill(3)
+# print(matching_zips_cbsas_df[matching_zips_cbsas_df['name'] == "Auburn University"])
+
+matching_zips_cbsas_df['tract'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(5,11)
+# print(matching_zips_cbsas_df[matching_zips_cbsas_df['name'] == "Auburn University"])
+
+match_df = matching_zips_cbsas_df[(matching_zips_cbsas_df["desired_state"] == matching_zips_cbsas_df["state"]) & (matching_zips_cbsas_df["desired_county"] == matching_zips_cbsas_df["county"])].drop_duplicates().copy()
+match_df['state'] = match_df['state'].astype(int)
+match_df['county'] = match_df['county'].astype(int)
+match_df['tract'] = match_df['tract'].astype(int)
+
+
+tract_puma_df['state'] = tract_puma_df['state'].astype(int)
+tract_puma_df['county'] = tract_puma_df['county'].astype(int)
+tract_puma_df['tract'] = tract_puma_df['tract'].astype(int)
+
+final_df = pd.merge(match_df, tract_puma_df, how="inner", on=["state","county","tract"])
 final_df = final_df.drop(columns=['desired_state','desired_county', 'year'])
 
-
-
-print(final_df.columns)
-print(final_df["name"].unique().shape)
 out_df = final_df[["name","state","county", "cbsa","puma"]].copy().drop_duplicates()
 filepath = Path("generated_data/college_FIPs.csv")
 out_df.to_csv(filepath, index=False)
