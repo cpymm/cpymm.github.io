92f6cdbd10e7d4c54810ef0577b75ec9482f4664
YourBoyConnor <connor.pymm@gmail.com>
2023-05-12 06:39:10 -0400

    census data collection done i think

--- ETL/census_client.py
+++ ETL/census_client.py
@@ -1,13 +1,11 @@
 import requests
 import pandas as pd
 import io
+from pathlib import Path 
 
 with open('census_api_key.txt') as key:
     api_key = key.read().strip()
-     
-with open('zip_to_tract_api_key.txt') as key:
-    zip_api_key = key.read().strip() 
-
+    
 # Go from school zips to tracts and MSAs/CBSAs
 school_df = pd.read_csv('generated_data/ScorecardData.csv')
 recent_sample = school_df[school_df["year"] == 2019].copy()
@@ -35,12 +33,15 @@ school_df["county_fips"] = school_df.apply(lambda row:
 zip_cbsa_tract_df = pd.read_csv('generated_data/ZipToCBSATract.csv')
 school_df['school.zip'] = school_df['school.zip'].str.slice(0,5).astype('int64')
 school_df.sort_values('school.zip')
-matching_zips_cbsas_df = school_df.merge(zip_cbsa_tract_df[['zip', 'tract', 'cbsa']], left_on='school.zip', right_on='zip')[['zip', 'tract', 'cbsa', 'year']]
-matching_zips_cbsas_df.columns = ['zip', 'tract', 'cbsa', 'year']
+
+matching_zips_cbsas_df = school_df.merge(zip_cbsa_tract_df[['zip', 'tract', 'cbsa']], left_on='school.zip', right_on='zip')[['school.name', 'zip', 'tract', 'cbsa', 'year', 'school.state_fips', 'county_fips']]
+matching_zips_cbsas_df.columns = ['name', 'zip', 'tract', 'cbsa', 'year', 'desired_state', 'desired_county']
 matching_zips_cbsas_df['state'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(0,2)
 matching_zips_cbsas_df['county'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(2,5)
 matching_zips_cbsas_df['tract'] = matching_zips_cbsas_df['tract'].astype('string').str.slice(5,11)
 matching_zips_cbsas_df.sort_values('tract')
+
+print(matching_zips_cbsas_df)
 
 # Get state and PUMA from tracts
 tract_puma_df = pd.read_table("source_data/2020_Census_Tract_to_2020_PUMA.txt", sep=",").astype('string')
@@ -50,40 +51,75 @@ final_df['state'] = final_df['STATEFP'].str.zfill(2)
 final_df['county'] = final_df['COUNTYFP'].str.zfill(3)
 final_df['puma'] = final_df['PUMA5CE']
 final_df = final_df.drop(columns=['STATEFP', 'COUNTYFP', 'TRACTCE', 'PUMA5CE'])
+final_df = final_df[(final_df['state'].astype('int64') == final_df['desired_state'].astype('int64')) & (final_df['county'].astype('int64') == final_df['desired_county'].astype('int64'))].reset_index(drop=True)
+final_df = final_df.drop(columns=['desired_state','desired_county'])
 
-final_df = final_df.drop_duplicates(subset=['zip', 'tract', 'year'], keep='first').reset_index(drop=True)
+# Get data I need from my final dataframe
+years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+states = ','.join(pd.unique(final_df['state'].astype('string')).tolist())
+pumas = final_df['puma'].astype('string')
+msas = ','.join(pd.unique(final_df['cbsa'].astype('string')).tolist())
 
-correct_states = pd.Series()
-correct_counties = pd.Series()
+for year in years:
+    # Create PUMA dataframe
+    dsource='acs'
+    dname='acs1'
+    cols='B19013_001E,B25105_001E'
+    state=states
+    puma=pumas
+    base_url = f'https://api.census.gov/data/{year}/{dsource}/{dname}'
+    data_url = f'{base_url}?get={cols}&for=public%20use%20microdata%20area:*&in=state:{state}&key={api_key}'
+
+    puma_response = requests.get(data_url)
+    puma_df = pd.read_csv(io.StringIO(puma_response.content.decode('utf-8')), on_bad_lines='skip')
+    puma_df = puma_df.drop(puma_df.columns[4], axis=1)
+    puma_df.columns = "median_household_income", "median_monthly_rent", "state_id", "puma"
+    puma_df["median_household_income"] = puma_df["median_household_income"].map(lambda i: ''.join([x for x in  i if x.isdigit()]))
+    puma_df["puma"] = puma_df["puma"].map(lambda i: ''.join([x for x in  i if x.isdigit()]))
+    puma_df = puma_df[puma_df['puma'].astype('int64').isin(pumas.astype('int64'))]
+    puma_df.to_csv(Path(f'generated_data/PUMA{year}.csv'), index=False)
+
+    # Create MSA dataframe
+    cols='NAME,B19013_001E,B25105_001E'
+    msa=msas
+    base_url = f'https://api.census.gov/data/{year}/{dsource}/{dname}'
+    data_url = f'{base_url}?get={cols}&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:{msa}&key={api_key}'
+
+    msa_response = requests.get(data_url)
+    msa_df = pd.read_csv(io.StringIO(msa_response.content.decode('utf-8')))
+    msa_df = msa_df.drop(msa_df.columns[4], axis=1)
+    msa_df.columns = "name", "median_household_income", "median_monthly_rent", "msa"
+
+    msa_df.to_csv(Path(f'generated_data/MSA{year}.csv'), index=False)
 
 # Create PUMA dataframe
 year='2021'
 dsource='acs'
 dname='acs1'
-cols='NAME,B19013_001E,B25105_001E'
-state='24'
-puma='01101'
+cols='B19013_001E,B25105_001E'
+state=states
+puma=pumas
 base_url = f'https://api.census.gov/data/{year}/{dsource}/{dname}'
-data_url = f'{base_url}?get={cols}&for=public%20use%20microdata%20area:{puma}&in=state:{state}&key={api_key}'
+data_url = f'{base_url}?get={cols}&for=public%20use%20microdata%20area:*&in=state:{state}&key={api_key}'
 
 puma_response = requests.get(data_url)
-puma_df = pd.read_csv(io.StringIO(puma_response.content.decode('utf-8')))
-puma_df.columns = "index", "name", "median_household_income", "median_monthly_rent", "state_id", "puma"
-
-print(puma_df)
-
-# Get MSA/CBSA from zips
-
+puma_df = pd.read_csv(io.StringIO(puma_response.content.decode('utf-8')), on_bad_lines='skip')
+puma_df = puma_df.drop(puma_df.columns[4], axis=1)
+puma_df.columns = "median_household_income", "median_monthly_rent", "state_id", "puma"
+puma_df["median_household_income"] = puma_df["median_household_income"].map(lambda i: ''.join([x for x in  i if x.isdigit()]))
+puma_df["puma"] = puma_df["puma"].map(lambda i: ''.join([x for x in  i if x.isdigit()]))
+puma_df = puma_df[puma_df['puma'].astype('int64').isin(pumas.astype('int64'))]
+puma_df.to_csv(Path('generated_data/PUMA2021.csv'), index=False)
 
 # Create MSA dataframe
 cols='NAME,B19013_001E,B25105_001E'
-msa='47900'
+msa=msas
 base_url = f'https://api.census.gov/data/{year}/{dsource}/{dname}'
 data_url = f'{base_url}?get={cols}&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:{msa}&key={api_key}'
 
 msa_response = requests.get(data_url)
 msa_df = pd.read_csv(io.StringIO(msa_response.content.decode('utf-8')))
-msa_df.columns = "index", "name", "median_household_income", "median_monthly_rent", "msa"
-msas = msa_df['msa']
+msa_df = msa_df.drop(msa_df.columns[4], axis=1)
+msa_df.columns = "name", "median_household_income", "median_monthly_rent", "msa"
 
-print(msa_df)
+msa_df.to_csv(Path('generated_data/MSA2021.csv'), index=False)
