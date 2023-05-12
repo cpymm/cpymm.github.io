a78538326fb76922be7f7dc4132578e35750e99b
Joe Diaz <jdiaz128@terpmail.umd.edu>
2023-05-12 03:30:19 -0400

    Absolutely evil manual data input for geo location

--- EDA/main.py
+++ EDA/main.py
@@ -3,8 +3,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 
-
-
 CollegeScorecard_df_raw = pd.read_csv("generated_data/ScorecardData.csv")
 scard_df = CollegeScorecard_df_raw.copy()
 scard_df = scard_df.rename(
--- ETL/census_client.py
+++ ETL/census_client.py
@@ -10,6 +10,28 @@ with open('zip_to_tract_api_key.txt') as key:
 
 # Go from school zips to tracts and MSAs/CBSAs
 school_df = pd.read_csv('generated_data/ScorecardData.csv')
+recent_sample = school_df[school_df["year"] == 2019].copy()
+desired_counties = [
+    "001", "081", "309", "007", "017", "025", "017", "049", "044", "037",
+    "003", "035", "027", "077", "059", "061", "109", "009", "063", "001", 
+    "121", "073", "005", "001", "001", "121", "063", "017", "001", "105", 
+    "510", "077", "037", "079", "017", "037", "013", "061", "183", 
+    "025", "031", "055", "037", "021", "157", "083", "201", "023", 
+    "085", "113", "085", "017", "103", "067", "041", "439", "453", 
+    "017", "071", "029", "001", "113", "059", "037", "047", "065", "073", 
+    "083", "087", "031", "013", "013", "003", "001", "059", "031", "019", "103",
+    "033", "015", "086", "161", "053", "063", "141",
+    "101", "003", "055", "073", "057", "037", "540",
+    "033", "025", "037", "045", "121", "067", "510",
+    "830", "047", "009", "061"]
+unis = recent_sample["school.name"].to_list()
+uni_county_pairs = list(zip(unis, desired_counties))
+uni_county_mapping = {uni: county for (uni, county) in uni_county_pairs}
+school_df["county_fips"] = school_df.apply(lambda row: 
+            uni_county_mapping[row["school.name"]],
+        axis=1
+)
+
 zip_cbsa_tract_df = pd.read_csv('generated_data/ZipToCBSATract.csv')
 school_df['school.zip'] = school_df['school.zip'].str.slice(0,5).astype('int64')
 school_df.sort_values('school.zip')
